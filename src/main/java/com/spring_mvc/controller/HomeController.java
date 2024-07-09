13b47b3ed7bb59c38e64f0b3febcdd603ce0d257
Rutvik Jasani <rutvikjasani96@gmail.com>
2024-07-09 11:22:05 +0530

    Sending data from controller using Model.

--- src/main/java/com/spring_mvc/controller/HomeController.java
+++ src/main/java/com/spring_mvc/controller/HomeController.java
@@ -1,13 +1,52 @@
 package com.spring_mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
     @RequestMapping("/home")
-    public String home(){
+    public String home(Model model){
         System.out.println("This is home url...");
+
+        model.addAttribute("name","Rutvik Jasani");
+        model.addAttribute("mobile","7984507354");
+
+        List<String> friends = new ArrayList<String>();
+        friends.add("Rutvik");
+        friends.add("Smit");
+        friends.add("Shivangi");
+        friends.add("Mansi");
+        friends.add("Rency");
+        friends.add("Jalpa");
+        friends.add("Sanket");
+        friends.add("Neel");
+        friends.add("Nikhil");
+        friends.add("Pratik");
+        friends.add("Meet");
+        friends.add("Kevin");
+        friends.add("Renish");
+
+        model.addAttribute("f",friends);
+
         return "home";
     }
+
+    @RequestMapping("/about")
+    public String about(){
+        System.out.println("About Page Called...");
+        return "about";
+    }
+
+    @RequestMapping("/contactus")
+    public String contactus(){
+        System.out.println("Contactus Page called...");
+        return "contactus";
+    }
+
+
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,4 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ page import="java.util.List" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,5 +8,29 @@
 <body>
     <h1>Hello Home</h1>
     <h2>Home Page Called...</h2>
+
+    <%
+        String name = (String) request.getAttribute("name");
+        String mobile = (String) request.getAttribute("mobile");
+        List<String> f = (List<String>) request.getAttribute("f");
+    %>
+
+    <h1>Name :  <%= name %></h1>
+    <h2>Mobile no:. <%= mobile %> </h2>
+
+    <h2>Friends</h2>
+    <%
+        if (f != null && !f.isEmpty()) {
+            for (String friend : f) {
+    %>
+                <h4><%= friend %></h4>
+    <%
+            }
+        } else {
+    %>
+            <h4>No friends found.</h4>
+    <%
+        }
+    %>
 </body>
 </html>
